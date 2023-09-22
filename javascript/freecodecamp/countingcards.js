fb0aff466273166890475f3a2704ed7f3519843a
josepedrolorenzini <joseplorenzini@gmail.com>
2023-09-22 12:50:34 +1000

    second exercies

--- javascript/freecodecamp/countingcards.js
+++ javascript/freecodecamp/countingcards.js
@@ -1,38 +1,38 @@
 let count = 0;
 
 function cc(card) {
-  // Only change code below this line
-  let result;
-  switch(card){
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-         count =  count + 1;
-         result =   count + ' bet ';
-         break;
-      case 7:
-      case 8:
-      case 9:
-          count = 0;
-          result =   count + ' hold ';
+    // Only change code below this line
+    let result;
+    switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+           count =  count + 1;
+           result =   count + ' Bet';
+           break;
+        case 7:
+        case 8:
+        case 9:
+          //  count = 0;
+            result =   count + ' Hold';
+            break;
+        case 10:
+        case 'J':
+        case 'Q': 
+        case 'K': 
+        case 'A':
+          count = count - 1;  
+          result =   count + ' Hold';
           break;
-      case 10:
-      case 'J':
-      case 'Q': 
-      case 'K': 
-      case 'A':
-        count = count - 1;  
-        result =   count + ' hold ';
-        break;
-          
+            
+    }
+  
+    return result;
+    // Only change code above this line
   }
-
-  return result;
-  // Only change code above this line
-}
-
+  
 console.log(cc(2));
 console.log(cc('K'));
 console.log(cc('A'));
