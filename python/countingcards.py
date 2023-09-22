e64405815209cfd4eef7d24cc2a17ccdc0390a13
josepedrolorenzini <joseplorenzini@gmail.com>
2023-09-22 12:37:25 +1000

    second exercies

--- python/countingcards.py
+++ python/countingcards.py
@@ -1,25 +1,23 @@
-count = 0 
 def cc():
-    answer = input('numero ')
-    card = answer
-    match card :
-        case 2:
-            result = 2
-        case 3:
-            result = 3
-        case 4:
-            result = 4
-        case 5:
-            result = 5
-        case 6:
-            result = 6
-        case _:
-            result = answer + ' numero '
-    print(result)
-    print(answer)
+    answer = input('Enter a number: ')
+    card = int(answer)  # Convert input to an integer
+    result = None  # Initialize result
+
+    # Use if-elif statements to match cases
+    if card == 2:
+        result = 2
+    elif card == 3:
+        result = 3
+    elif card == 4:
+        result = 4
+    elif card == 5:
+        result = 5
+    elif card == 6:
+        result = 6
+    else:
+        result = str(answer) + ' numero '
+
+    print('Result:', result)
+    print('Answer:', answer)
 
 cc()
-
-
-
-   
