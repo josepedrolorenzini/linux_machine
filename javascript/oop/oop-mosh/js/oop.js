d8bb04bad982c017c6b4ee5988a16e7f08ff426b
josepedrolorenzini <joseplorenzini@gmail.com>
2023-11-17 12:25:42 +1100

    oop javascript

---
+++ javascript/oop/oop-mosh/js/oop.js
@@ -0,0 +1,9 @@
+class Test{
+   static consoLog(){
+        return this.name;
+    }
+}
+
+
+console.log(Test.consoLog())
+/// https://github.com/bradtraversy/javascript-sandbox/blob/main/08-shopping-list-project/02-remove-clear-items/shopping-list/script.js
